d169b07de23ba325bc39e833d17d3b31327ab9a0
Rafael Berger <Rafiberger613@gmail.com>
2017-11-17 00:26:09 -0500

    Config stuff

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,9 @@ namespace AThirdCarDealership
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+        //    services.AddDbContext<ApplicationDbContext>(options =>
+          //      options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 

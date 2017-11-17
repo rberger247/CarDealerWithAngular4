a7f992054fef4b5ba1b2ae02e4b470875233d515
Rafael Berger <Rafiberger613@gmail.com>
2017-11-17 00:57:42 -0500

    added solution

---
+++ Models/Model.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using thirdCarDealership.Models;
+
+namespace AthirdCarDealership.Models
+{
+    [Table("Models")]
+    public class Model
+    {
+       public int Id { get; set; }
+       
+       [Required]
+       [StringLength(255)]
+       public string Name { get; set; } 
+
+       public Make Make { get; set; }
+
+       public int MakeId { get; set; }
+    }
+}
